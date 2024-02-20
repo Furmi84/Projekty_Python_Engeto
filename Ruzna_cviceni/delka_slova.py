ac4beaaceb9cec6aa557cc7e3a834ef317a5f2a3
Furmi84 <martin.furmanek@gmail.com>
2024-02-20 09:03:09 +0100

    Presani kodu a overeni fukčnosti

---
+++ Ruzna_cviceni/delka_slova.py
@@ -0,0 +1,19 @@
+# Zadaná slova
+zadana_slova = ["Matous", "Martin", "ahoj", "er", "es", "i", "a"]
+
+slovo = zadana_slova[6]
+delka_slova = len(slovo)
+
+if delka_slova >= 4:
+    print("velikost slova",slovo,"je: ", delka_slova)
+
+elif 3 > delka_slova > 1:
+    print("velikost slova",slovo,"je 2")
+    
+elif delka_slova == 1:
+    print ("velikost slova",slovo,"je 1")
+
+
+
+
+
