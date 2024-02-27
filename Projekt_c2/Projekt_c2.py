e7d20cb38fc8aa48915172d170e8fed161084bc7
Furmi84 <martin.furmanek@gmail.com>
2024-02-27 15:06:15 +0100

    Tdms to CSV

--- Projekt_c2/Projekt_c2.py
+++ Projekt_c2/Projekt_c2.py
@@ -5,9 +5,24 @@ email: martin.furmanek@gmail.com
 discord: Furmi84
 """
 
-data = {
-    "force":[1.1,1.2,1,1],
-    "presurre":[1.1,1.2,1,1],
-}
+import glob
+from nptdms import TdmsFile
 
-print (data["force"])
+def prace_se_soubory():
+    
+    files = glob.glob('C:/Users/MartinFurmanek/Documents/Projekty_Python/Projekt_c2/Data/**/*Results.tdms',  
+                   recursive = True) 
+    return files
+
+def vycteni_data(soubory):
+    return 
+
+def analyza_dat():
+    return 
+
+def export_do_csv():
+    return
+
+soubory = prace_se_soubory()
+
+print(prace_se_soubory())
